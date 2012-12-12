755cb7dcafd3e65f7b2a74add88de9fa03885331
themikekowalski <themikekowalski@gmail.com>
2012-12-12 15:29:28 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Floorsbyhunter::Application.config.secret_token = '6b8cc68e0643b32c0440c23edeec181627727f802674c84b0e528f58ff3042c7c1d103648b1397688f2372f87fc7b2974fc82bd939092d23504e4ceb6d89742d'
