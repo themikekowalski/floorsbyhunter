e14a9e7536c3284532b1fe7fc6aeda5de482a2de
themikekowalski <themikekowalski@gmail.com>
2012-12-12 16:48:17 -0400

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def links
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Floorsbyhunter::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/links"
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
